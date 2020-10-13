45166e0fa1d7ecce7057f3d19282a22102fc0e3e
GetPython2020 <72427707+GetPython2020@users.noreply.github.com>
2020-10-12 19:20:23 -0600

    Add files via upload

---
+++ SQL_Create_Salesperson.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+# only run one time
+#c.execute(''' CREATE TABLE Salesperson (
+#            ID integer,
+ #           Name text,
+  #          Age integer,
+   #         Salary integer)
+   #     ''')
+#only run one time
+c.execute("DELETE FROM Salesperson")
+conn.commit()
+c.execute('''INSERT INTO Salesperson VALUES
+            (1, 'Abe', 61, 140000),
+            (2, 'Bob', 34, 44000),
+            (5, 'Chris', 34, 4000),
+            (7, 'Dan', 41, 52000),
+            (8, 'Ken', 57, 115000),
+            (11, 'Joe', 38, 38000)
+            ''')
+conn.commit()
+
+c.execute("SELECT * FROM Salesperson")
+print(c.fetchall())
+
+conn.commit()  # execute the sql command
+conn.close()
