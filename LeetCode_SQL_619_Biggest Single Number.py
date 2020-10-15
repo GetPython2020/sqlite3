f472c16cbffaec2c33c4b70a35c566c1059be36d
GetPython2020 <72427707+GetPython2020@users.noreply.github.com>
2020-10-15 11:52:31 -0600

    Update LeetCode_SQL_619_Biggest Single Number.py

--- LeetCode_SQL_619_Biggest Single Number.py
+++ LeetCode_SQL_619_Biggest Single Number.py
@@ -5,21 +5,21 @@ import sqlite3
 conn = sqlite3.connect('data_L.db')
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE number(
- #          num integer)
-  #       ''')
-#c.execute('''INSERT INTO number VALUES
- #           (1),
-  #          (8),
-   #         (7),
-    #        (4),
-     #       (1),
-      #      (2),
-       #     (3),
-        #    (4),
-         #   (8),
-          #  (7)
-           # ''')
+c.execute('''CREATE TABLE number(
+           num integer)
+         ''')
+c.execute('''INSERT INTO number VALUES
+            (1),
+            (8),
+            (7),
+            (4),
+            (1),
+            (2),
+            (3),
+            (4),
+            (8),
+            (7)
+            ''')
 
 c.execute("SELECT max(num) from (SELECT num FROM number GROUP BY num Having COUNT(*)=1")
 print(c.fetchall())
